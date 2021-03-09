dcb365431fe46b0ab5d5a502b57c97ef0a0b4399
Sahil Khanna <sahilsan@gmail.com>
2021-03-09 12:49:14 -0800

    Initial pathfind changes

--- src/Actors/EnemyBase.gd
+++ src/Actors/EnemyBase.gd
@@ -11,11 +11,19 @@ onready var animation_player: AnimationPlayer = $AnimationPlayer
 export (PackedScene) var Bullet = load("res://src/Actors/Bullet.tscn")
 export var stationary: = false
 
-var path: = PoolVector2Array() setget set_path
+# Constants
+var SPEED := 100.0
+var PATH_TIMER_CD := 0.2
+
+# Global Vars
 var health: = 100
-var speed = 100.0
 var movement_vector = Vector2.ZERO
 var can_shoot: = false
+
+# Pathfinding Global Vars
+var follow_path: = PoolVector2Array() setget set_path
+var path_timer = PATH_TIMER_CD
+var current_path_node = 0
 
 func _ready() -> void:
 	anim_sprite.play()
@@ -29,17 +37,44 @@ func _physics_process(delta: float) -> void:
 	if not stationary:
 		var player = get_parent().get_node("Player")
 		if player:
+
+			path_timer -= delta
+			if path_timer <= 0.0 or follow_path.size() == 0:
+				path_timer = PATH_TIMER_CD
+				print("Path reset")
+
+				# Path optimization set to 'false' so that entities don't get stuck on walls.
+				follow_path = nav_2d.get_simple_path(self.global_position, player.global_position, false)
+				current_path_node = 0
+			elif current_path_node > follow_path.size() -1:
+				# TODO Check if this is needed.
+				return
+
+
 		# TODO: This is temporary motion, replace with better once you do pathfinding.
 		# Smaller the divisor, the faster the enemy.
 			#movement_vector = position.direction_to(player.position) * speed * delta
 			#move_and_collide(movement_vector)
 			var distance_from_player = self.global_position.distance_to(player.global_position)
 			if distance_from_player > 150:
-				var new_path: PoolVector2Array = nav_2d.get_simple_path(self.global_position, player.global_position)
+				# var new_path: PoolVector2Array = nav_2d.get_simple_path(self.global_position, player.global_position)
 				#line_2d.points = new_path
-				path = new_path
-				var move_distance: float = speed * delta
-				move_along_path(move_distance)
+				# path = new_path
+				# move_along_path_2(delta)
+				#var move_distance: float = speed * delta
+				#move_along_path(move_distance)
+				var direction = (follow_path[current_path_node] - self.global_transform.origin).normalized()
+				move_and_slide(direction * SPEED)
+				# move_and_collide(direction * delta * SPEED)
+
+				# Current path point has been reached, go to next.
+				if follow_path[current_path_node].distance_to(self.global_transform.origin) < 1.0:
+					# if current_path_node != follow_path.size():
+						# current_path_node += 1
+					if current_path_node < follow_path.size() - 3:
+						current_path_node += 3
+					elif current_path_node != follow_path.size():
+						current_path_node += 1
 			else:
 				if can_shoot:
 					shoot()
@@ -64,25 +99,41 @@ func shoot() -> void:
 		can_shoot = false
 
 func set_path(value: PoolVector2Array) -> void:
-	path = value
+	follow_path = value
 	#if value.size() == 0:
 	#	return
 
-func move_along_path(distance: float) -> void:
-	var start_point: = position
-	for _idx in range(self.path.size()):
-		var distance_to_next: = start_point.distance_to(path[0])
-		if distance <= distance_to_next and distance >= 0.0:
-			position = start_point.linear_interpolate(path[0], distance/distance_to_next)
-			## TODO: use move_and_collide instead of updating the position
-			#var new_position = start_point.linear_interpolate(path[0], distance/distance_to_next)
-			#move_and_collide(new_position)
-			break
-		elif distance < 0.0:
-			position = path[0]
-		distance -= distance_to_next
-		start_point = path[0]
-		path.remove(0)
+# func move_along_path_2(delta: float) -> void:
+	# var start_point: = position
+	# for _idx in range(self.path.size()):
+		# var distance_to_next: = start_point.distance_to(path[0])
+		# if distance <= distance_to_next and distance >= 0.0:
+			# # position = start_point.linear_interpolate(path[0], distance/distance_to_next)
+			# ## TODO: use move_and_collide instead of updating the position
+			# #var new_position = start_point.linear_interpolate(path[0], distance/distance_to_next)
+			# #move_and_collide(new_position)
+			# break
+		# elif distance < 0.0:
+			# position = path[0]
+		# distance -= distance_to_next
+		# start_point = path[0]
+		# path.remove(0)
+
+# func move_along_path(distance: float) -> void:
+	# var start_point: = position
+	# for _idx in range(self.path.size()):
+		# var distance_to_next: = start_point.distance_to(path[0])
+		# if distance <= distance_to_next and distance >= 0.0:
+			# position = start_point.linear_interpolate(path[0], distance/distance_to_next)
+			# ## TODO: use move_and_collide instead of updating the position
+			# #var new_position = start_point.linear_interpolate(path[0], distance/distance_to_next)
+			# #move_and_collide(new_position)
+			# break
+		# elif distance < 0.0:
+			# position = path[0]
+		# distance -= distance_to_next
+		# start_point = path[0]
+		# path.remove(0)
 
 func hit(damage: int) -> void:
 	# https://www.davidepesce.com/2019/11/25/godot-tutorial-11-attacks-damage-death/
--- src/Actors/StrongRanged.gd
+++ src/Actors/StrongRanged.gd
@@ -3,5 +3,5 @@ extends "res://src/Actors/EnemyBase.gd"
 func _ready() -> void:
 	print("Updating self values.")
 	self.health = 200
-	self.speed = 80.0
+	self.SPEED = 80.0
 	label.text = "%s" % health
